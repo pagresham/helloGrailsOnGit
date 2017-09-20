86d38c7317bdccad1be2bc9a79410b5ec17224a7
Pierce Gresham <gresham@VSAC.org>
2017-09-20 11:53:40 -0400

    initial commit

---
+++ grails-app/init/hellogitgrails/Application.groovy
@@ -0,0 +1,10 @@
+package hellogitgrails
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
